385a8fc2c3a574e09fc35e4332cf7f19362e5c37
Paul Cox <pauldanielcox@gmail.com>
2011-04-05 14:51:21 +0200

    adding gps parsing

--- biketest/scripts/utils/mti.pl
+++ biketest/scripts/utils/mti.pl
@@ -3,10 +3,23 @@
 #Use as follows with MTIG:
 # MTIHardTest /dev/ttyUSB0 -o 2 -d 6 -v | perl mti.pl
 
+my $time0 = 0;
+my $cnt = 0;
 
 while (<>){
-	$_ =~ /^(.*)\sACC.*EUL(.*)POS/;
-	$time = $1;
+
+	$cnt++;
+	$_ =~ /^(.*)\sACC.*EUL(.*)POS\s+(.*)\s+31T\sVEL/;
+	my $gps = $3;	
+	if ($time0){
+		$time = $1 - $time0;
+	} else {
+		$time0 = $1;
+	}
 	$2 =~ /\s+(.+)\s+(.+)\s+(.+)/;
-	print printf "%f %f %f %f\n",$time, $1, $2, $3;
+	next if ($cnt%50);	
+	printf "%06.2f att: %06.2f %06.2f %06.2f ",$time, $1, $2, $3;
+	$gps =~ /(\S+)\s(\S+)\s+(.+)/;
+	#printf $gps."\n";	
+	printf "gps: %06.2f %06.2f %06.2f\n", $1, $2, $3;
 }
